fd38a53c6856119b96ecec8bafdd9e8e3ee77d53
Jacob Gold <goldjacobe@gmail.com>
2023-03-31 14:57:34 -0400

    feat: new chrome extension (#9)
    ![Screenshot 2023-03-30 at 17.23.35.png](https://graphite-user-uploaded-assets-stg.s3.amazonaws.com/rMe0Vo8L75bPjoZxC4rb/0290bc19-aff4-429c-82d7-67f322b159f4/Screenshot%202023-03-30%20at%2017.23.35.png)pdating the icon upstack

--- src/background.ts
+++ src/background.ts
@@ -1,47 +1,4 @@
 /**
- * Pause button
- */
-
-const UNPAUSE_ALARM_NAME = 'UNPAUSE_ALARM';
-let paused = false;
-
-chrome.action.onClicked.addListener(function (tab) {
-  if (paused) {
-    unpause();
-  } else {
-    pause();
-    paused = true;
-    chrome.alarms.create(UNPAUSE_ALARM_NAME, {
-      delayInMinutes: 5,
-    });
-  }
-});
-
-chrome.alarms.onAlarm.addListener((alarm) => {
-  if (alarm.name === UNPAUSE_ALARM_NAME) {
-    unpause();
-  }
-});
-
-function pause() {
-  void chrome.declarativeNetRequest.updateEnabledRulesets({
-    disableRulesetIds: ['primary_ruleset'],
-  });
-  void chrome.action.setBadgeText({ text: 'pause' });
-}
-
-function unpause() {
-  void chrome.declarativeNetRequest.updateEnabledRulesets({
-    enableRulesetIds: ['primary_ruleset'],
-  });
-  void chrome.action.setBadgeText({ text: '' });
-  if (paused) {
-    void chrome.alarms.clear(UNPAUSE_ALARM_NAME);
-  }
-  paused = false;
-}
-
-/**
  * Allow site to see if chrome extension exists
  */
 
@@ -52,5 +9,4 @@ chrome.runtime.onMessageExternal.addListener(function (
 ) {
   sendResponse({ ok: true });
 });
-
 export {};
---
+++ src/pr-content-script.ts
@@ -0,0 +1,70 @@
+const VIEW_ON_GRAPHITE_ID = 'view-on-graphite';
+const TAB_ICON =
+  '<svg style="margin-left: 6px; vertical-align: text-bottom;" xmlns="http://www.w3.org/2000/svg" height=16 width=16 viewBox="0 0 16 16"><path fill="currentColor" d="M9.7932,1.3079L3.101,3.101l-1.7932,6.6921,4.899,4.899,6.6921-1.7931,1.7932-6.6921L9.7932,1.3079Zm1.0936,11.6921H5.1133l-2.8867-5L5.1133,3h5.7735l2.8867,5-2.8867,5Z"/><polygon fill="currentColor" points="11.3504 4.6496 6.7737 3.4232 3.4232 6.7737 4.6496 11.3504 9.2263 12.5768 12.5768 9.2263 11.3504 4.6496"/></svg>';
+
+function addViewOnGraphiteButton() {
+  if (document.getElementById(VIEW_ON_GRAPHITE_ID)) {
+    return;
+  }
+
+  const tabs = document.querySelector('[aria-label="Pull request tabs"]');
+  if (!tabs) {
+    return;
+  }
+
+  const a = document.createElement('a');
+  a.id = VIEW_ON_GRAPHITE_ID;
+  a.className = 'tabnav-tab flex-shrink-0';
+  a.href = document.URL.replace('github.com', 'app.graphite.dev/github');
+  a.innerHTML = 'View this PR on Graphite' + TAB_ICON;
+
+  tabs.appendChild(a);
+}
+
+const MERGE_WARNING_ID = 'graphite-merge-warning';
+const WARNING_ICON =
+  '<svg style="margin-right: 4px; vertical-align: text-bottom;" xmlns="http://www.w3.org/2000/svg" height=20 width=20 viewBox="0 0 16 16"><path fill="currentColor" d="M9.7932,1.3079L3.101,3.101l-1.7932,6.6921,4.899,4.899,6.6921-1.7931,1.7932-6.6921L9.7932,1.3079Zm1.0936,11.6921H5.1133l-2.8867-5L5.1133,3h5.7735l2.8867,5-2.8867,5Z"/><polygon fill="currentColor" points="11.3504 4.6496 6.7737 3.4232 3.4232 6.7737 4.6496 11.3504 9.2263 12.5768 12.5768 9.2263 11.3504 4.6496"/></svg>';
+
+function addMergeWarning() {
+  if (document.getElementById(MERGE_WARNING_ID)) {
+    return;
+  }
+
+  const merge = document.getElementsByClassName('merge-message')[0];
+  if (!merge) {
+    return;
+  }
+
+  const span = document.createElement('span');
+  span.innerText = 'Merge this pull request on the Graphite dashboard';
+  span.className = 'h4 status-heading color-fg-attention';
+
+  const a = document.createElement('a');
+  a.innerHTML = WARNING_ICON;
+  a.id = MERGE_WARNING_ID;
+  a.style.color = 'inherit';
+  a.style.marginBottom = '8px';
+  a.style.display = 'block';
+  a.href =
+    document.URL.replace('github.com', 'app.graphite.dev/github') +
+    '?merge-modal=true';
+  a.appendChild(span);
+
+  merge.prepend(a);
+}
+
+function addGraphiteElements() {
+  console.log('addGraphiteElements');
+  addViewOnGraphiteButton();
+  addMergeWarning();
+}
+
+new MutationObserver(addGraphiteElements).observe(document.body, {
+  childList: true,
+  attributes: true,
+  subtree: true,
+});
+
+addGraphiteElements();
+
+export {};
